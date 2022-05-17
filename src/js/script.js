f53784d387c70d04db83736574f66a2f8325033e
Marge <marge.consunji@gmail.com>
2022-05-17 17:39:25 +0800

    Toggle share option classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 console.log('test script');
+
+const shareButton = document.querySelector('.article__details__share__button');
+const shareOptions = document.querySelector(
+  '.article__details__share__options'
+);
+
+const handleShare = () => {
+  console.log('firing');
+  shareOptions.classList.toggle('active');
+};
